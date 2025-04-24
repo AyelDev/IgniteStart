038c91d410c08762038f59e311650ab789e7038f
ariel abelgas <aarielabelgas23@gmail.com>
2025-04-24 18:25:35 +0800

    Comprehending routing mechanisms and controller redirections

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends BaseController
+{
+    public function getstudents(): void
+    {
+        
+        $db = \Config\Database::connect();
+        $query = $db->query("SELECT * FROM user");
+        $result = $query->getResult();
+
+        // Set the correct header for JSON output
+        header('Content-Type: application/json');
+
+        // Output the result as JSON
+        echo json_encode($result);
+            // return view('index_file');
+    }
+}
