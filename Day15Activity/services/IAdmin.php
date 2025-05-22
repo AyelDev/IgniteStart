c3a735ae17bbddff1458b089324e59508c001b5a
AyelDev <aarielabelgas23@gmail.com>
2025-05-22 15:26:29 +0800

    clean temp

--- Day15Activity/services/IAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface IAdmin{
-    //this function for adding admin
-    public function addAdmin($name, $email, $username, $password):bool;
-
-    // this function for viewing admins
-    public function getAdmins():array;
-}
--- Day15Activity/views/admin/nav_buttons.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<a href="../../views/admin/dashboard.php" class="dashboard">Dashboard</a>
-<a href="../../views/admin/profile.php" class="profile">Profile</a>
-<a href="../../controllers/MainController.php?logout=true" class="logout">Logout</a>';
